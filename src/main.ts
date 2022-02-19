4008e8327d7f8a028413ab452ec8a5405c6d3523
phibr0 <bronzel.phillip@gmail.com>
2022-02-19 20:03:00 +0100

    Compatibility with CM6

--- src/main.ts
+++ src/main.ts
@@ -1,91 +1,123 @@
-import * as CodeMirror from 'codemirror';
-import { Editor, MarkdownView, Plugin } from 'obsidian';
-import { TaboutSettingsTab } from './ui/settings';
-import { TaboutSettings, DEFAULT_SETTINGS } from './types';
-import RuleCreateModal from './ui/ruleCreateModal';
+import * as CodeMirror from "codemirror";
+import { Editor, MarkdownView, Plugin } from "obsidian";
+import { TaboutSettingsTab } from "./ui/settings";
+import { TaboutSettings, DEFAULT_SETTINGS } from "./types";
+import RuleCreateModal from "./ui/ruleCreateModal";
+import { keymap } from "@codemirror/view";
+import { EditorState, Extension, Prec } from "@codemirror/state";
+import { syntaxTree } from "@codemirror/language";
 
 export default class TaboutPlugin extends Plugin {
-	settings: TaboutSettings;
-	handlingMath: boolean;
+  settings: TaboutSettings;
+  legacy: boolean;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		this.addSettingTab(new TaboutSettingsTab(this.app, this));
+    //@ts-expect-error `config` isn't available in the type defs
+    this.legacy = this.app.vault.config.legacyEditor;
+    if (this.legacy) {
+      //Use the legacy tabout for CodeMirror 5
+      this.registerCodeMirror((cm: CodeMirror.Editor) => {
+        cm.on("beforeChange", this.legacyTabout);
+      });
+    } else {
+      //Use a proper Editor Extension for CodeMirror 6
+      this.registerEditorExtension(
+        Prec.high(
+          keymap.of([
+            {
+              key: "Tab",
+              run: (eView) => this.tabout(this.getToken(eView.state)),
+            },
+          ])
+        )
+      );
+    }
 
-		this.addCommand({
-			id: "tabout-add-rule-here",
-			name: "Add Rule for this Environment",
-			editorCallback: (editor: Editor) => {
-				//@ts-expect-error
-				new RuleCreateModal(this, editor.cm.getTokenTypeAt(editor.getCursor()) ?? "").open();
-			}
-		});
+    this.addSettingTab(new TaboutSettingsTab(this.app, this));
 
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			cm.on("beforeChange", this.handleTabs);
-		});
-	}
+    this.addCommand({
+      id: "tabout-add-rule-here",
+      name: "Add Rule for this Environment",
+      editorCallback: (editor: Editor) => {
+        let token = "";
+        if (this.legacy) {
+          //@ts-expect-error cm is not defined in the type docs
+          token = editor.cm.getTokenTypeAt(editor.getCursor());
+        } else {
+          //@ts-expect-error cm is not defined in the type docs
+          token = this.getToken(editor.cm.state);
+        }
+        new RuleCreateModal(
+          this,
+          token
+        ).open();
+      },
+    });
+  }
 
-	handleTabs = (cm: CodeMirror.Editor, changeObj: CodeMirror.EditorChange) => {
-		if (changeObj.text.first() === "	") {
-			for (let rule of this.settings.rules) {
-				// If Cursor is in correct environment
-				if (!rule.tokenMatcher || cm.getTokenTypeAt(cm.getCursor())?.contains(rule.tokenMatcher)) {
-					// Get Cursor Position
-					const pos = cm.getCursor();
-					// Get content of Line after Cursor
-					const afterCursor = cm.getLine(pos.line).substring(pos.ch);
-					// Determine the nearest character
-					const nextChar = Math.min(...this.getIndices(rule.lookups, afterCursor, rule.jumpAfter));
-					// If there is a nearest one jump right after it
-					if (nextChar != Infinity) {
-						// @ts-ignore Don't insert the Tab
-						changeObj.cancel();
-						cm.setCursor(pos.line, pos.ch + nextChar);
-					}
-				}
-			}
-		}
-	};
+  getToken = (state: EditorState) => {
+    const ast = syntaxTree(state);
+    return ast.resolveInner(state.selection.main.head, -1).type
+      .name as string;
+  }
 
-	getIndices(rules: string[], afterCursor: string, jumpAfter: boolean) {
-		let n: number[] = [];
-		rules.forEach(r => {
-			let idx = afterCursor.indexOf(r);
-			if (idx != -1) {
-				n.push(jumpAfter ? idx + r.length : idx);
-			}
-		});
-		return n;
-	}
+  tabout = (token: string): boolean => {
+    for (let rule of this.settings.rules) {
+      if (token.contains(rule.tokenMatcher)) {
+        const editor =
+          this.app.workspace.getActiveViewOfType(MarkdownView).editor;
+        // Get Cursor Position
+        const pos = editor.getCursor();
+        // Get content of Line after Cursor
+        const afterCursor = editor.getLine(pos.line).substring(pos.ch);
+        // Determine the nearest character
+        const nextChar = Math.min(
+          ...this.getIndices(rule.lookups, afterCursor, rule.jumpAfter)
+        );
+        // If there is a nearest one jump right after it
+        if (nextChar != Infinity) {
+          editor.setCursor(pos.line, pos.ch + nextChar);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 
-	//This is needed to also work when Obsidian is set to using Spaces instead of Tabs. (doesnt work yet haha)
-	getTabString(ch: number): string {
-		//@ts-expect-error
-		if (this.app.vault.getConfig("useTab")) {
-			return "";
-		} else {
-			let tab = "";
-			//@ts-expect-error
-			const tabSize: number = this.app.vault.getConfig("tabSize");
-			let remaining = ch-tabSize;
-			for(let i = 0; i < remaining; i++) {
-				tab += " ";
-			}
-			return tab;
-		}
-	}
+  legacyTabout = (cm: CodeMirror.Editor, changeObj: CodeMirror.EditorChange) => {
+    if (changeObj.text.first() === "	") {
+      const token = cm.getTokenTypeAt(cm.getCursor());
+      //@ts-expect-error
+      if (this.tabout(token)) changeObj.cancel();
+    }
+  };
 
-	onunload() {
-		this.app.workspace.iterateCodeMirrors(cm => cm.off("beforeChange", this.handleTabs));
-	}
+  getIndices(rules: string[], afterCursor: string, jumpAfter: boolean) {
+    let n: number[] = [];
+    rules.forEach((r) => {
+      let idx = afterCursor.indexOf(r);
+      if (idx != -1) {
+        n.push(jumpAfter ? idx + r.length : idx);
+      }
+    });
+    return n;
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  onunload() {
+    if (this.legacy) {
+      this.app.workspace.iterateCodeMirrors((cm) =>
+        cm.off("beforeChange", this.legacyTabout)
+      );
+    }
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
